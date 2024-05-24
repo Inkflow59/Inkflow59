99c592e7f757afc4b31e660aa52ffef7ea8a3b21
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2024-05-24 11:14:49 +0200

    Rajout du modèle TS

---
+++ codeSection/code.ts
@@ -0,0 +1,63 @@
+// Language : TypeScript
+const name :string = "Tom";
+const pseudo :string = "Inkflow";
+var age :number = 20;
+
+function languagesMastered() :string[] {
+    var masteredTechnos :string[] = [
+        "Clickteam Fusion",
+        "HTML5",
+        "CSS3",
+        "JavaScript",
+        "TypeScript",
+        "PHP",
+        "MySQL",
+        "SQLite",
+        "Java",
+        "Python"
+    ];
+    return masteredTechnos;
+}
+
+var hosTeam :boolean = true
+function isOrganizationPresident(hosTeam :boolean) :boolean {
+    if (hosTeam==true) {
+        return true;
+    }   else {
+        return false;
+    }
+}
+
+function currentStatus() :string[] {
+    var formations :any[] = [
+        ["2023 - 2025",
+        "BTS SIO (SLAM)",
+        "Gaston Berger"]
+    ];
+
+    var workplace :any[] = [
+        "Student"
+    ];
+
+    if(workplace[0]=="Student") {
+        return formations;
+    }   else {
+        return workplace;
+    }
+}
+
+function currentProjects() :any[] {
+    var projects :any[] = [
+        "CreaSQL",
+        "PortInkFolio",
+        "HoS Website",
+        "Heroes of Songs : J.V",
+        "Mysterious Bot: Reinitialized"
+    ];
+    return projects;
+}
+
+languagesMastered();
+isOrganizationPresident(hosTeam);
+currentStatus();
+currentProjects();
